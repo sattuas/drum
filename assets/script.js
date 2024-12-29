430b1b6498e7211ec0c23cf3931e8c69a1cbb8bf
raphael <rafael.dks10@gmail.com>
2024-12-29 15:08:07 -0300

    sound keys updated

--- assets/script.js
+++ assets/script.js
@@ -4,8 +4,19 @@ document.addEventListener('keydown', (event) => {
 
 function playSound(sound) {
     let audioElement = document.querySelector(`#s_${sound}`);
+    let keyElement = document.querySelector(`input[data-key="${sound}"]`);
 
     if(audioElement) {
+        audioElement.currentTime = 0;
         audioElement.play();
+    }
+
+    if(keyElement) {
+        // console.log(keyElement);
+        keyElement.classList.add('active');
+
+        setTimeout(() => {
+            keyElement.classList.remove('active');
+        }, 300);
     }
 }
